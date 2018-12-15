317e8f87732f38a22bafae7224aa6dda240bf8f4
rabbit216 <1074926956@qq.com>
2018-12-16 00:02:53 +0800

    实现所有房子信息的插入

--- src/com/lwj/house/HouseSearch.java
+++ src/com/lwj/house/HouseSearch.java
@@ -6,38 +6,133 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 
 
 public class HouseSearch {
-    final static String url2_1 = "http://61.142.120.214:9000/web/realestate_presale.jsp?" +
-            "licenceCode=%BB%DD%CD%E5%B7%BF%D4%A4%D0%ED%D7%D6%A1%BE2018%A1%BF%B5%DA166%BA%C5&ProjectCode=DYW00446001";
-    final static String url3_1 = "http://61.142.120.214:9000/web/salestable.jsp?buildingcode=DYW0044600101&projectcode=DYW00446001";
-    final static String url4Head = "http://61.142.120.214:9000/web/House.jsp?";
-    final static String url3Head = "http://61.142.120.214:9000/web/";
+    static int conut = 0;
+    final static String urlHead = "http://61.142.120.214:9000/web/";
+    final static String homePreSale = urlHead + "salepermit.jsp?page=";
+    final static String url4Head = urlHead + "House.jsp?";
 
     public static void main(String[] args) throws IOException {
-        ArrayList<String> url3List = getUrl3List(url2_1);
-        int conut = 0;
-        for (int i = 0; i < url3List.size(); i++) {
-            String url3 = url3List.get(i);
-            ArrayList<String> url4List = getUrl4List(url3);
-            for (int j = 0; j < url4List.size(); j++) {
-                String url4 = url4List.get(j);
-                Document document4 = getDocument(url4);
-                HouseInfo houseInfo = changeDoumentToHouse(document4);
-                inserHouseInfoToMysql(houseInfo);
-                conut = conut + 1;
-            }
-            System.out.println("此次共获取"+conut+"信息");
+        getAllHouseInfo();
+    }
+
+    /**
+     * 获取所有的房子信息
+     *
+     * @throws IOException
+     */
+    private static void getAllHouseInfo() throws IOException {
+        //获得20页的链接
+        for (int pager = 1; pager < 20; pager++) {
+            //每一页的链接
+            System.out.println("---------第" + pager + "页--------------");
+            String url1 = homePreSale + pager;
+            getUrl2Foreach2(url1);
+            System.out.println("共获取" + conut + "信息");
         }
+    }
+
+    /**
+     * 得到并遍历第二层链接的集合
+     *
+     * @param url1 第一层的链接 样式:http://61.142.120.214:9000/web/salepermit.jsp?page=1
+     * @throws IOException
+     */
+    private static void getUrl2Foreach2(String url1) throws IOException {
+        //第二层的链接集合
+        ArrayList<String> url2List = getUrl2List(url1);
+        //遍历第二层的集合
+        for (int i = 0; i < url2List.size(); i++) {
+            String url2 = url2List.get(i);
+            getUrl3Foreach3(url2);
+        }
+    }
+
+    /**
+     * 得到并遍历第三层链接的集合
+     *
+     * @param url2 第二层的链接 样式:http://61.142.120.214:9000/web/realestate_presale.jsp?licenceCode=%BB%DD%CD%E5%B7%BF%D4%A4%D0%ED%D7%D6%A1%BE2018%A1%BF%B5%DA168%BA%C5&ProjectCode=DYW00071057
+     * @throws IOException
+     */
+
+    private static void getUrl3Foreach3(String url2) throws IOException {
+        //第三层的链接集合
+        ArrayList<String> url3List = getUrl3List(url2);
+        //遍历第三层的集合
+        for (int k = 0; k < url3List.size(); k++) {
+            String url3 = url3List.get(k);
+            getUrl4Foreach4(url3);
+        }
+    }
+
+    /**
+     * 得到并遍历第四层链接的集合
+     *
+     * @param url3 第三层的链接 样式:http://61.142.120.214:9000/web/salestable.jsp?buildingcode=DYW0007105701&projectcode=DYW00071057
+     * @throws IOException
+     */
+
+    private static int getUrl4Foreach4(String url3) throws IOException {
+        //第四层的链接集合
+        ArrayList<String> url4List = getUrl4List(url3);
+        //遍历第四层的集合
+        for (int j = 0; j < url4List.size(); j++) {
+            String url4 = url4List.get(j);
+            insertUrl4(url4);
+        }
+        return conut;
+    }
+
+    /**
+     * 解析并插入第四层的房子信息
+     *
+     * @param url4 样式：http://61.142.120.214:9000/web/House.jsp?id=399455&lcStr=0
+     * @throws IOException
+     */
+    private static void insertUrl4(String url4) throws IOException {
+        //得到document对象
+        Document document4 = getDocument(url4);
+        //得到houseinfo对象
+        HouseInfo houseInfo = changeDoumentToHouse(document4);
+        //把houseinfo对象插入到数据库
+        inserHouseInfoToMysql(houseInfo);
+        conut = conut + 1;
+        System.out.println("此次共获取" + conut + "信息");
+    }
+
+
+    /**
+     * 通过url1拿到url2的集合
+     *
+     * @param url1 链接一
+     * @return ArrayList<String> url2集合
+     * @throws IOException
+     */
+    public static ArrayList<String> getUrl2List(String url1) throws IOException {
+        Document doc = Jsoup.parse(new URL(url1).openStream(), "GBK", url1);
+        Elements links = doc.select("a[target=_blank]");
+        ArrayList<String> urlList1 = new ArrayList<>();
+        for (Element link : links) {
+            String linkHref = link.attr("href");
+            String url = urlHead + linkHref;
+            if (url.contains("licenceCode")) {
+//               System.out.println("getUrl2List=" + url);
+                String shiftURL = getShiftURL(url);
+                urlList1.add(shiftURL);
+
+            }
+        }
+        return urlList1;
     }
 
     /**
@@ -74,7 +169,7 @@ public class HouseSearch {
         ArrayList<String> urlList2 = new ArrayList<>();
         for (Element link : links) {
             String linkHref = link.attr("href");
-            urlList2.add(url3Head + linkHref);
+            urlList2.add(urlHead + linkHref);
         }
         return urlList2;
     }
@@ -93,6 +188,7 @@ public class HouseSearch {
         return url4Head + id + ends;
     }
 
+
     /**
      * 传入一个HouseInfo对象将其插入到数据库
      *
@@ -109,10 +205,10 @@ public class HouseSearch {
         String sql = "insert into house_list(projectName,building,room,price,houseArea) values(" + "'" + projectName + "'" + ","
                 + "'" + building + "'" + "," + "'" + room + "'" + "," + "'" + price + "'" + "," + "'" + houseArea + "'" + ")";
         try {
-            System.out.println("sql=" + sql);
+//            System.out.println("sql=" + sql);
             Statement statement = (Statement) conn.createStatement();    // 创建用于执行静态sql语句的Statement对象
             int count = statement.executeUpdate(sql);  // 执行插入操作的sql语句，并返回插入数据的个数
-            System.out.println("向表中插入" + count + "条数据成功"); //输出插入操作的处理结果
+//            System.out.println("向表中插入" + count + "条数据成功"); //输出插入操作的处理结果
             conn.close();   //关闭数据库连接
         } catch (SQLException e) {
             System.out.println("插入数据失败" + e.getMessage());
@@ -127,7 +223,7 @@ public class HouseSearch {
      * @throws IOException
      */
     private static Document getDocument(String url) throws IOException {
-        System.out.println(url);
+//        System.out.println(url);
         return Jsoup.connect(url).get();
     }
 
@@ -258,5 +354,40 @@ public class HouseSearch {
         }
         return con; //返回所建立的数据库连接
     }
+
+    /**
+     * @param str url2=http://61.142.120.214:9000/web/realestate_presale.jsp?licenceCode=惠湾房预许字【2018】第168号&ProjectCode=DYW00071057
+     * @return 拼接后的url2=url2=http://61.142.120.214:9000/web/realestate_presale.jsp?licenceCode=%BB%DD%CD%E5%B7%BF%D4%A4%D0%ED%D7%D6%A1%BE2018%A1%BF%B5%DA168%BA%C5&ProjectCode=DYW00071057
+     */
+
+    private static String getShiftURL(String str) {
+        String str3 = new String(str);
+//        System.out.println("getShiftURL=" + str);
+        String[] arr = str3.split("=|&");
+        String url2 = arr[0] + "=" + getURLEncoderString(arr[1]) + "&" + arr[2] + "=" + arr[3];
+        return url2;
+    }
+
+    /**
+     * URL 转码
+     *
+     * @return String 惠湾房预许字【2018】第168号
+     * @author lifq %BB%DD%CD%E5%B7%BF%D4%A4%D0%ED%D7%D6%A1%BE2018%A1%BF%B5%DA168%BA%C5
+     * @date 2015-3-17 下午04:10:28
+     */
+    public static String getURLEncoderString(String str) {
+        String result = "";
+        if (null == str) {
+            return "";
+        }
+        try {
+            result = java.net.URLEncoder.encode(str, "GBK");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
+
+
 
